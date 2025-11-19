2ad55a0c7ed5a5fe072413b1468572704937322c
Rio Stokes <riostokes@riostokes.com>
2025-11-19 01:47:20 +0000

    Initial Commit

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Rio Stokes <riostokes@riostokes.com>'
+description 'Lightweight Food Delivery Job for ESX'
+
+shared_scripts {
+
+}
+
+client_scripts {
+
+}
+
+server_scripts {
+
+}
+
+
+dependencies {}
