cbc551d79ddb58d2606390bfea995701f671d07a
Rio Stokes <riostokes@riostokes.com>
2025-11-20 23:12:10 +0000

    Add Vehicle Notifications & Clocked In Check

--- client/vehicles.lua
+++ client/vehicles.lua
@@ -1,22 +1,25 @@
 RegisterNetEvent('rs_fooddelivery:spawnDeliveryVehicle')
-AddEventHandler('rs_fooddelivery:spawnDeliveryVehicle', function(vehicleModel)
+AddEventHandler('rs_fooddelivery:spawnDeliveryVehicle', function(vehicleModel, canSpawnVehicle)
 
-    RequestModel(vehicleModel)
-    while not HasModelLoaded(vehicleModel) do
-        Wait(10)
-    end
+    if canSpawnVehicle then
+        RequestModel(vehicleModel)
+        while not HasModelLoaded(vehicleModel) do
+            Wait(10)
+        end
 
-    local vehicle = CreateVehicle(
-    vehicleModel,
-    Config.FoodDeliveryRentVehicleSpaces.coords.x,
-    Config.FoodDeliveryRentVehicleSpaces.coords.y,
-    Config.FoodDeliveryRentVehicleSpaces.coords.z,
-    Config.FoodDeliveryRentVehicleSpaces.heading,
-    true,
-    false
-    )
+        local vehicle = CreateVehicle(
+        vehicleModel,
+        Config.FoodDeliveryRentVehicleSpaces.coords.x,
+        Config.FoodDeliveryRentVehicleSpaces.coords.y,
+        Config.FoodDeliveryRentVehicleSpaces.coords.z,
+        Config.FoodDeliveryRentVehicleSpaces.heading,
+        true,
+        false
+        )
 
-    SetVehicleOnGroundProperly(vehicle)
-
-    ESX.ShowNotification("Successfully Rented Vehicle", "success", 3000, "Vehicle Notification")
+        SetVehicleOnGroundProperly(vehicle)
+        ESX.ShowNotification("Successfully Rented Vehicle", "success", 3000, "Vehicle Notification")
+    else
+        ESX.ShowNotification("You must be clocked in to rent a vehicle.", "error", 3000, "Vehicle Notification")
+    end  
 end)
--- server/vehicles.lua
+++ server/vehicles.lua
@@ -4,13 +4,20 @@ RegisterNetEvent('rs_fooddelivery:rentDeliveryVehicle')
 AddEventHandler('rs_fooddelivery:rentDeliveryVehicle', function()
     local src = source
     local xPlayer = ESX.GetPlayerFromId(src)
+    local canSpawnVehicle = false
 
     if xPlayer then
-        local cashBalance = xPlayer.getMoney()
+        local currentPlayerJob = xPlayer.getJob().name
+
+        if currentPlayerJob == 'fooddelivery' then
+            local cashBalance = xPlayer.getMoney()
   
-        if cashBalance > Config.FoodDeliveryRentVehicleCost then
-            xPlayer.removeMoney(Config.FoodDeliveryRentVehicleCost, 'Rent Delivery Vehicle')
-            TriggerClientEvent('rs_fooddelivery:spawnDeliveryVehicle', src, Config.FoodDeliveryRentVehicle)
+            if cashBalance > Config.FoodDeliveryRentVehicleCost then
+                xPlayer.removeMoney(Config.FoodDeliveryRentVehicleCost, 'Rent Delivery Vehicle')
+                canSpawnVehicle = true
+            end
         end
+
+        TriggerClientEvent('rs_fooddelivery:spawnDeliveryVehicle', src, Config.FoodDeliveryRentVehicle, canSpawnVehicle)
     end
 end)
